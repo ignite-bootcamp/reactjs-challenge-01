f34472ad6794e28690453ec2bdb3d5e1b7f7a178
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-27 01:07:33 -0300

    feat: page header with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { PlusCircledIcon } from "@radix-ui/react-icons";
-import { Button } from "./components/button";
-import { Checkbox } from "./components/checkbox";
-import { Input } from "./components/input";
-import { DeleteButton } from "./components/delete-button";
-import { Task } from "./components/task";
-import { useState } from "react";
-
+import logo from "./assets/Logo.svg";
 function App() {
-  const [checked, setChecked] = useState(true);
   return (
-    <div>
-      <Checkbox onCheckedChange={console.log} />
-      <Input onChange={console.log} />
-      <Button onClick={console.log}>
-        Criar
-        <PlusCircledIcon className="w-4 h-4 ml-2" />
-      </Button>
-      <DeleteButton onClick={console.log} />
-      <Task
-        completed={checked}
-        onClick={() => setChecked(prev => !prev)}
-        id="taskId"
-      >
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </Task>
-    </div>
+    <section>
+      <header className="bg-gray-700 h-52">
+        <img src={logo} className="mx-auto pt-16" />
+      </header>
+    </section>
   );
 }
 
