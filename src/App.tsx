e6dff27bb383492847910d6f0e5df6fe8d6481d0
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-27 01:14:26 -0300

    feat: adding a form with input and button to create a task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { PlusCircledIcon } from "@radix-ui/react-icons";
 import logo from "./assets/Logo.svg";
+import { Input } from "./components/input";
+import { Button } from "./components/button";
 function App() {
   return (
     <section>
       <header className="bg-gray-700 h-52">
         <img src={logo} className="mx-auto pt-16" />
       </header>
+      <div className="max-w-[736px] mx-auto">
+        <form className="flex gap-2 -mt-7 px-2">
+          <Input onChange={console.log} />
+          <Button>
+            Criar
+            <PlusCircledIcon className="ml-2" />
+          </Button>
+        </form>
+      </div>
     </section>
   );
 }
